33783f19ad480496e1d1e629e07c8aa4bcf4dd05
Gabby Suazo <gabby.suazo@gmail.com>
2020-08-06 17:53:00 -0400

    Add setTimeout function to change initial text into corresponding colors

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    setTimeout(function(){
+        $("#box1-text").html("Pink");
+        $("#box2-text").html("Blue"); 
+        $("#box3-text").html("Green"); 
+        $("#box4-text").html("Yellow"); 
+    }, 2000);
+});
