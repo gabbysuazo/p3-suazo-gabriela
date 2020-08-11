8c186c9f8df8466a0f83581cac41253805d35902
Gabby Suazo <gabby.suazo@gmail.com>
2020-08-11 09:59:53 -0400

    Adjust javascript to run color function only once

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ $(document).ready(function(){
         $("#box4-text").html("Yellow"); 
     }, 2000);
 
-    var box1 = document.getElementById("box-1");
-    var box2 = document.getElementById("box-2");
-    var box3 = document.getElementById("box-3");
-    var box4 = document.getElementById("box-4");
+    var box1 = document.getElementsByClassName("box-1")[0];
+    var box2 = document.getElementsByClassName("box-2")[0];
+    var box3 = document.getElementsByClassName("box-3")[0];
+    var box4 = document.getElementsByClassName("box-4")[0];
+    var firstClick = 0;
 
-    $(box1).click(function(){
+
+    $(box1).one("click", function pinkAnimation(){
         $(box1).animate({left: '50%'}, 1000);
         $(box2).animate({top: '50%'}, 1000);
         $(box3).animate({top: '0%'}, 1000);
@@ -36,11 +38,12 @@ $(document).ready(function(){
             $("#box1-text").html("3");
             $("#box2-text").html("5"); 
             $("#box3-text").html("7"); 
-            $("#box4-text").html("9"); 
+            $("#box4-text").html("9");
+            firstClick = 1;
         }, 4000);
     });
 
-    $(box2).click(function(){
+    $(box2).one("click", function blueAnimation(){
         $(box1).animate({left: '50%'}, 1000);
         $(box2).animate({top: '50%'}, 1000);
         $(box3).animate({top: '0%'}, 1000);
@@ -65,11 +68,12 @@ $(document).ready(function(){
             $("#box1-text").html("3");
             $("#box2-text").html("5"); 
             $("#box3-text").html("7"); 
-            $("#box4-text").html("9"); 
+            $("#box4-text").html("9");
+            firstClick = 1;
         }, 4000);
     });
 
-    $(box3).click(function(){
+    $(box3).one("click", function greenAnimation(){
         $(box1).animate({left: '50%'}, 1000);
         $(box2).animate({top: '50%'}, 1000);
         $(box3).animate({top: '0%'}, 1000);
@@ -99,11 +103,12 @@ $(document).ready(function(){
             $("#box1-text").html("5");
             $("#box2-text").html("9"); 
             $("#box3-text").html("3"); 
-            $("#box4-text").html("7"); 
+            $("#box4-text").html("7");
+            firstClick = 1;
         }, 5000);
     });
 
-    $(box4).click(function(){
+    $(box4).one("click", function yellowAnimation(){
         $(box1).animate({left: '50%'}, 1000);
         $(box2).animate({top: '50%'}, 1000);
         $(box3).animate({top: '0%'}, 1000);
@@ -138,27 +143,35 @@ $(document).ready(function(){
             $("#box1-text").html("9");
             $("#box2-text").html("7"); 
             $("#box3-text").html("5"); 
-            $("#box4-text").html("3"); 
+            $("#box4-text").html("3");
+            firstClick = 1;
         }, 6000);
     });
 
-/*
-    $("#box-2")
-            .animate({top: '50%'}, 1000)
-            .animate({left: '0%'}, 1000)
-            .animate({top: '0%'}, 1000)
-            .animate({left: '50%'}, 1000);
+    if (firstClick == 1) {
+        $(box1).on("click", function pinkAnimation(){
+            $(box1).animate({left: '50%'}, 1000);
+            $(box2).animate({top: '50%'}, 1000);
+            $(box3).animate({top: '0%'}, 1000);
+            $(box4).animate({left: '0%'}, 1000);
 
-            $("#box-3")
-            .animate({top: '0%'}, 1000)
-            .animate({left: '50%'}, 1000)
-            .animate({top: '50%'}, 1000)
-            .animate({left: '0%'}, 1000);
+            $(box1).animate({top: '50%'}, 1000);
+            $(box2).animate({left: '0%'}, 1000);
+            $(box3).animate({left: '50%'}, 1000);
+            $(box4).animate({top: '0%'}, 1000);
 
-            $("#box-4")
-            .animate({left: '0%'}, 1000)
-            .animate({top: '0%'}, 1000)
-            .animate({left: '50%'}, 1000)
-            .animate({top: '50%'}, 1000);
-*/
+            $(box1).animate({left: '0%'}, 1000);
+            $(box2).animate({top: '0%'}, 1000);
+            $(box3).animate({top: '50%'}, 1000);
+            $(box4).animate({left: '50%'}, 1000);
+        });
+        $(box2).on("click", function blueAnimation(){
+        });
+        $(box3).on("click", function greenAnimation(){
+        });
+        $(box4).on("click", function yellowAnimation(){
+        });
+    } else {
+        console.log('Pick a Color. Choose Wisely.');
+    }
 });
