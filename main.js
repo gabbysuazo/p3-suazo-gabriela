3b44db29dd0942196aab7c185d4928f10bc9b095
Gabby Suazo <gabby.suazo@gmail.com>
2020-08-12 17:10:39 -0400

    Add fortunes and randomize them when inserted into innerhtml

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ $(document).ready(function(){
     var box4 = document.getElementsByClassName("box-4")[0];
     var click = 0;
 
+    const messages = [
+        'A lifetime of happiness lies ahead of you.', 
+        'The end of something marks the start of something new.', 
+        'Self-knowledge is a life long process.', 
+        'The person who will not stand for something will fall for anything.',
+        'The value lies not within any particular thing, but in the desire placed on that thing.',
+        'When your heart is pure, your mind is clear.',
+        'You have the power to write your own fortune.',
+        'Success is a journey, not a destination.',
+        'Nothing is more difficult, and therefore more precious, than to be able to decide.',
+        'It’s not the amount of time you devote, but what you devote to the time that counts.'
+    ];
+    const randomIndex = Math.round(Math.random()*messages.length);
+
     $(box1).click(function(){
         if (click == 0) {
             click = click + 1;
@@ -84,7 +98,7 @@ $(document).ready(function(){
             $("#box4-text").fadeOut();
 
             setTimeout(function(){
-                $("#box1-text").html("temprary message 1");
+                $("#box1-text").html(messages[randomIndex]);
                 document.getElementById("box1-text").style.backgroundColor = "#ffffff";
                 document.getElementById("box1-text").style.color = "#C879DB";
                 document.getElementById("box1-text").style.fontSize = "2rem";
@@ -181,7 +195,7 @@ $(document).ready(function(){
             $("#box4-text").fadeOut();
 
             setTimeout(function(){
-                $("#box2-text").html("temprary message 2");
+                $("#box2-text").html(messages[randomIndex]);
                 document.getElementById("box2-text").style.backgroundColor = "#ffffff";
                 document.getElementById("box2-text").style.color = "#7070ED";
                 document.getElementById("box2-text").style.fontSize = "2rem";
@@ -282,7 +296,7 @@ $(document).ready(function(){
             $("#box4-text").fadeOut();
 
             setTimeout(function(){
-                $("#box3-text").html("temprary message 3");
+                $("#box3-text").html(messages[randomIndex]);
                 document.getElementById("box3-text").style.backgroundColor = "#ffffff";
                 document.getElementById("box3-text").style.color = "#54BABA";
                 document.getElementById("box3-text").style.fontSize = "2rem";
@@ -388,7 +402,7 @@ $(document).ready(function(){
             $("#box4-text").fadeOut();
 
             setTimeout(function(){
-                $("#box4-text").html("temprary message 4");
+                $("#box4-text").html(messages[randomIndex]);
                 document.getElementById("box4-text").style.backgroundColor = "#ffffff";
                 document.getElementById("box4-text").style.color = "#E8D48F";
                 document.getElementById("box4-text").style.fontSize = "2rem";
